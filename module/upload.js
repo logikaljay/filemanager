fa66684e6d43f80e9bbf1e309e6768ccb021c27d
Jay <logikal@outlook.com>
2013-10-31 11:14:26 +1300

    Added POST api structure for uploading a file

--- module/upload.js
+++ module/upload.js
@@ -1,5 +1,19 @@
+var uuid = require('node-uuid'),
+        fs = require('fs'),
+        error = { upload: "error" },
+        success = { upload: "success" };
+
 module.exports=function(app) {
 	app.get('/upload', function(req, res) {
 		res.send('/upload called');
+	});
+
+	app.post('/upload', function(req, res) {
+		var file = res.body.file;
+		if (file != undefined && file.length) {
+			// file exists
+		} else {
+			// file doesn't exist
+		}
 	});
 }
