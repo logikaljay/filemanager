bd9660ea0ad7d6c781e774a29dbb719e48a7c8c2
Jay <logikal@outlook.com>
2013-10-31 15:46:22 +1300

    added module/download.js new api functions for downloading a file from a container

---
+++ module/download.js
@@ -0,0 +1,39 @@
+var uuid = require('node-uuid'),
+        fs = require('fs'),
+        mime = require('mime'),
+        async = require('async'),
+        error = { download: "error" },
+        success = { download: "success" };
+
+var containers = './containers/';
+
+module.exports=function(app) {
+	app.post('/download/:container/:file', function(req, res) {
+		var key = req.body.key;
+		var container = req.params.container;
+		var file = req.params.file;
+		if ((key != undefined && key.length) && (container != undefined && container.length)) {
+			var path = getFile(container, key, file);
+			if (!path.message) {
+				res.download(path, file);
+			} else {
+				res.json(path);
+			}
+		} else {
+			error.message = "container and/or key not supplied";
+			res.json(error);
+		}
+	});
+}
+
+function getFile(container, key, file) {
+	var path = containers + key + "/" + container +"/"+ file;
+	var exists = fs.existsSync(path);
+	if (exists) {
+		var file = path;
+		return file;
+	} else {
+		error.message = "file not found";
+		return error;
+	}
+}
