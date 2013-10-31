24481382f6b143074a407ac9959b33f5f7e0c48d
Jay <logikal@outlook.com>
2013-10-31 13:26:30 +1300

    Extended container module container module now can list containers and files in a container. when listing the files in the container, stats and mime type are pulled out also.

--- module/container.js
+++ module/container.js
@@ -1,11 +1,58 @@
 var uuid = require('node-uuid'),
 	fs = require('fs'),
+	mime = require('mime'),
+	async = require('async'),
 	error = { create: "error" },
 	success = { create: "success" };
 
 var containers = './containers/';
 
 module.exports=function(app) {
+		/**
+		 * POST - /container/list/ - Get the containers
+		 * param - key - the api key for the user
+		 */
+		app.post('/container/list', function(req, res) {
+			var key = req.body.key;
+			if (key != undefined && key.length) {
+				success = { list: "success" };
+				success.containers = getContainers(key);
+				res.json(success);
+			} else {
+				error.message = "invalid key";
+				res.json(error);
+			}
+		});
+		
+		app.post('/container/:container/list', function(req, res) {
+			var key = req.body.key;
+			if (key != undefined && key.length) {
+				success = { list: "success" };
+				success.container = req.params.container;
+				success.files = [];
+				var items = getContainerFiles(success.container, key);
+				async.eachSeries(items, function(item, callback) {
+					//var path = containers + key +"/" + success.container + "/" + item;
+					//var stats = fs.statSync(path);
+					//var type = mime.lookup(path);
+					var output = getContainerFileStats(success.container, key, item);
+					success.files.push(output);
+					callback();
+				}, function(err) {
+					res.json(success);
+				});
+			} else {
+				error = { list: "error" };
+				error.message = "invalid key";
+				res.json(error);
+			}
+		});
+
+		/**
+		 * POST - /container/create - Create a container
+		 * param - container - The name of the container
+		 * param - key - the api key for the user
+		 */
         app.post('/container/create', function(req, res) {
 				var container = req.body.container;
 				var key = req.body.key;
@@ -17,6 +64,45 @@ module.exports=function(app) {
 					res.json(error);
 				}
         });
+}
+
+function getContainers(key) {
+	var exists = fs.existsSync(containers + key);
+	if (exists) {
+		var items = fs.readdirSync(containers + key);
+		return items;
+	} else {
+		error = { list: "error" };
+		error.message = "incorrect key";
+		return error;
+	}
+}
+
+function getContainerFiles(container, key) {
+	var containerExists = fs.existsSync(containers + key + "/" + container);
+	if (containerExists) {
+		var output = [];
+		var items = fs.readdirSync(containers + key + "/" + container);
+		return items;
+	} else {
+		error = { list: "error" };
+		error.message = "incorrect container and/or key";
+		return error;
+	}
+}
+
+function getContainerFileStats(container, key, file) {
+	var path = containers + key + "/" + container + "/" + file;
+	var fileExists = fs.existsSync(path);
+	if (fileExists) {
+		var stats = fs.statSync(path);
+		var type = mime.lookup(path);
+		return { item: file, size: stats.size, mtime: stats.mtime, type: type };
+	} else {
+		error = { list: "error" };
+		error.message = "file(s) doesn't exist";
+		return error;
+	}
 }
 
 function createContainer(name, key) {
@@ -36,6 +122,7 @@ function createContainer(name, key) {
 				error.message = "cannot create a container";
 				return error;
 			} else {
+				success = { create: "success" };
 				success.name = name;
 				success.key = key;
 				return success;
