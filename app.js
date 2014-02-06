4bee40eaaecd5369b82ffe180837f837843087ed
Jay <logikal@gmail.com>
2014-02-07 10:52:18 +1300

    created new field for common.paths, jshinted container core module

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 "use strict";
 
-var schemas = "./schema/",
-    core    = "./core/",
-    modules = "./module/";
-
 var express = require("express"),
 	fs = require("fs"),
 	mongoose = require("mongoose");
@@ -12,9 +8,16 @@ mongoose.connect("mongodb://localhost/filemanager");
 
 var common = {
     app: express(),
-    mongoose: require("mongoose")
+    mongoose: require("mongoose"),
+    path: {
+        containers: "./containers/",
+        schemas: "./schema/",
+        core: "./core/",
+        modules: "./module/"
+    }
 };
 
+// setup express
 var allowCrossDomain = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -30,23 +33,26 @@ common.app.configure(function() {
 	common.app.use(express.session({secret: "QWERTY123456789"}));
 });
 
+// Load any schemas
 common.schemas = {};
-fs.readdirSync(schemas).forEach(function(file) {
-    var schema = schemas + file;
+fs.readdirSync(common.path.schemas).forEach(function(file) {
+    var schema = common.path.schemas + file;
     require(schema)(common);
 });
 
+// Load core modules
 common.core = {};
-fs.readdirSync(core).forEach(function(file) {
-    var module = core + file;
+fs.readdirSync(common.path.core).forEach(function(file) {
+    var module = common.path.core + file;
     require(module)(common);
 });
 
+// Load non-essential modules
 common.modules = {};
-fs.readdirSync(modules).forEach(function(file) {
-	var module = modules + file;
+fs.readdirSync(common.path.modules).forEach(function(file) {
+	var module = common.path.modules + file;
 	require(module)(common);
 });
 
-console.log("listening for connections on " + (process.env.PORT || 9123));
+// start express
 common.app.listen(process.env.PORT || 9123);
--- core/container.js
+++ core/container.js
@@ -1,45 +1,52 @@
-var uuid = require('node-uuid'),
-	fs = require('fs'),
-	fsExtra = require('fs.extra'),
-	mime = require('mime'),
-	async = require('async'),
+"use strict";
+
+var fs = require("fs"),
+	mime = require("mime"),
+	async = require("async"),
 	error = { container: "error" },
 	success = { container: "success" };
 
-var containers = './containers/';
-
 module.exports = function(common) {
+	var containers = common.path.containers;
+
     var app = common.app;
     
 	/**
 	 * POST - /container/list/ - Get the containers
 	 * @param {string} key The api key for the user
 	 */
-	app.get('/container/list', function(req, res) {
+	app.get("/container/list", function(req, res) {
 		var key = req.query.key;
 		if (key !== undefined && key.length) {
 			success = { container: "success" };
-			var containers = getContainers(key);
 			success.containers = [];
-			var User = common.mongoose.model('User', common.schemas.user);
-			var Container = common.mongoose.model('Container', common.schemas.container);
-			Container.find().populate({
-			        path: '_creator',
-			        match: { key: key }
-			    }).exec(function(err, containers) {
-			        console.log(containers);
-			    async.eachSeries(containers, function(item, callback) {
-			        if (item._creator !== null) {
-                        success.containers.push({
-                            name: item.name 
-                        });
-			        }
-			        
-                    callback();
-                }, function(err) {
-                    res.json(success);
-                });
-			});
+			var User = common.mongoose.model("User", common.schemas.user);
+			if (User === null) {
+				res.json({error: "could not list containers"});
+			} else {
+				var Container = common.mongoose.model("Container", common.schemas.container);
+				Container.find().populate({
+				        path: "_creator",
+				        match: { key: key }
+				    }).exec(function(err, containers) {
+				        console.log(containers);
+				    async.eachSeries(containers, function(item, callback) {
+				        if (item._creator !== null) {
+	                        success.containers.push({
+	                            name: item.name
+	                        });
+				        }
+				        
+	                    callback();
+	                }, function(err) {
+						if (err) {
+							res.json({error: "could not list containers"});
+						} else {
+							res.json(success);
+						}
+	                });
+				});
+			}
 		} else {
 			error.message = "invalid key";
 			res.json(error);
@@ -51,40 +58,42 @@ module.exports = function(common) {
 	 * param - container - The name of the container
 	 * param - key - the api key for the user
 	*/
-	app.get('/container/:container/list', function(req, res) {
+	app.get("/container/:container/list", function(req, res) {
 		var key = req.query.key;
 		if (key !== undefined && key.length) {
 			success.container = req.params.container;
 			success.files = [];
-			var User = common.mongoose.model('User', common.schemas.user);
+			var User = common.mongoose.model("User", common.schemas.user);
 			User.findByApi(key, function(err, user) {
 			    if (user === null) {
 			        res.json({error: "invalid api key"});
 			    } else {
-    			    var File = common.mongoose.model('File', common.schemas.file);
-        			var Container = common.mongoose.model('Container', common.schemas.container);
-        			Container.findByApiAndName(key, req.params.container, function(err, container) {
-        			    console.log(container);
-        			    if (container.length < 1) {
-        			        res.json({error: "couldn't find container"});
-        			    }
-            			File.find().populate({
-            			    path: '_creator',
-            			    match: { name: req.params.container }
-            			}).exec(function(err, files, container) {
-            			    async.eachSeries(files, function(file, callback) {
-            			        if (file._creator !== null) {
-            			            console.log(file);
-            			            var fileName = file.name;
-            			            var output = getContainerFileStats(req.params.container, user._id, fileName);
-            			            success.files.push(output);
-            			        }
-            			        callback();
-            			    }, function(err) {
-            			        res.json(success);
-            			    });
-            			});
-        			});
+					var File = common.mongoose.model("File", common.schemas.file);
+					var Container = common.mongoose.model("Container", common.schemas.container);
+					Container.findByApiAndName(key, req.params.container, function(err, container) {
+						if (container.length < 1) {
+							res.json({error: "couldn't find container"});
+						}
+						File.find().populate({
+							path: "_creator",
+							match: { name: req.params.container }
+						}).exec(function(err, files) {
+							async.eachSeries(files, function(file, callback) {
+								if (file._creator !== null) {
+									var fileName = file.name;
+									var output = getContainerFileStats(req.params.container, user._id, fileName);
+									success.files.push(output);
+								}
+								callback();
+							}, function(err) {
+								if (err) {
+									res.json({error: "could not find container"});
+								} else {
+									res.json(success);
+								}
+							});
+						});
+					});
 			    }
 			});
 		} else {
@@ -98,11 +107,11 @@ module.exports = function(common) {
 	 * param - container - The name of the container
 	 * param - key - the api key for the user
 	 */
-	app.get('/container/create/:container', function(req, res) {
+	app.get("/container/create/:container", function(req, res) {
 		var container = req.params.container;
 		var key = req.query.key;
 		
-        var User = common.mongoose.model('User', common.schemas.user);
+        var User = common.mongoose.model("User", common.schemas.user);
         User.findByApi(key, function(err, user) {
             if (user) {
                 createContainer(common.container, container, user._id, function(result) {
@@ -113,71 +122,49 @@ module.exports = function(common) {
             }
         });
 	});
-};
 
-function getContainers(key) {
-	var exists = fs.existsSync(containers + key);
-	if (exists) {
-		var items = fs.readdirSync(containers + key);
-		return items;
-	} else {
-		error.message = "incorrect key";
-		return error;
-	}
-};
-
-function getContainerFiles(container, key) {
-	var containerExists = fs.existsSync(containers + key + "/" + container);
-	if (containerExists) {
-		var items = fs.readdirSync(containers + key + "/" + container);
-		return items;
-	} else {
-		error.message = "incorrect container and/or key";
-		return error;
-	}
-}
-
-function getContainerFileStats(container, userId, file) {
-	var path = containers + userId + "/" + container + "/" + file;
-	var fileExists = fs.existsSync(path);
-	if (fileExists) {
-		var stats = fs.statSync(path);
-		var type = mime.lookup(path);
-		return { item: file, size: stats.size, mtime: stats.mtime, type: type };
-	} else {
-		error.message = "file doesn't exist";
-		return error;
-	}
-}
-
-function createContainer(db, name, userId, cb) {
-	var keyExists = fs.existsSync(containers + userId);
-	if (!keyExists) {
-		var createKey = fs.mkdirSync(containers + userId);
-		if (createKey) {
-			error.message = "cannot create a container";
-            cb(error);
+	function getContainerFileStats(container, userId, file) {
+		var path = containers + userId + "/" + container + "/" + file;
+		var fileExists = fs.existsSync(path);
+		if (fileExists) {
+			var stats = fs.statSync(path);
+			var type = mime.lookup(path);
+			return { item: file, size: stats.size, mtime: stats.mtime, type: type };
 		} else {
-			var createName = fs.mkdirSync(containers + userId + "/" + name);
-			if (createName) {
+			error.message = "file doesn't exist";
+			return error;
+		}
+	}
+
+	function createContainer(db, name, userId, cb) {
+		var keyExists = fs.existsSync(containers + userId);
+		if (!keyExists) {
+			var createKey = fs.mkdirSync(containers + userId);
+			if (createKey) {
 				error.message = "cannot create a container";
+	            cb(error);
+			} else {
+				var createName = fs.mkdirSync(containers + userId + "/" + name);
+				if (createName) {
+					error.message = "cannot create a container";
+					cb(error);
+				} else {
+	                db.createContainer(name, userId, function(err, result) {
+	                    cb(result);
+	                });
+				}
+			}
+		} else {
+			var exists = fs.existsSync(containers + userId + "/" + name);
+			if (exists) {
+				error.message = "container already exists";
 				cb(error);
 			} else {
-                db.createContainer(name, userId, function(err, result) {
-                    cb(result);
-                });
+				fs.mkdirSync(containers + userId + "/" + name);
+	            db.createContainer(name, userId, function(result) {
+	                cb(result);
+	            });
 			}
 		}
-	} else {
-		var exists = fs.existsSync(containers + userId + "/" + name);
-		if (exists) {
-			error.message = "container already exists";
-			cb(error);
-		} else {
-			fs.mkdirSync(containers + userId + "/" + name);
-            db.createContainer(name, userId, function(result) {
-                cb(result);
-            });
-		}
 	}
-}
+};
